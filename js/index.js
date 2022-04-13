693514327da3dfc97b7b2c0767300ee7a0cac861
MD5678 <93808560+MD5678@users.noreply.github.com>
2022-04-12 22:42:43 -0600

    hovers

---
+++ js/index.js
@@ -0,0 +1,6 @@
+
+console.log("Your index.js folder is loaded correctly!");
+// $(".navigation").hover(function(){
+//     console.log("hover")
+//     $(this).css("background-color","#A1C19A");
+// });
